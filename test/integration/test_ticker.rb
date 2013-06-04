cf837166f636d2bb21face937f4b4ae8a53f6ab9
Viraj Parikh <virajparikh@gmail.com>
2013-06-04 09:04:19 -0500

    passed first ticker integration test

--- test/integration/test_ticker.rb
+++ test/integration/test_ticker.rb
@@ -3,32 +3,60 @@ require_relative '../test_helper'
 class TestTickers < MiniTest::Unit::TestCase
   include DatabaseCleaner  ## what is this?
 
-  # def test_9_add_ticker
-  #   portfolio = Ticker.create(name: "Tech")
-  #   `ruby rubystocks add Tech`
-  #   assert_equal(1, ticker.count)
+  def test_9_add_tickers
+    Portfolio.create!(name: "Tech")
+    shell_output = ""
+    IO.popen('ruby rubystocks edit Tech', 'r+') do |pipe|
+      pipe.puts("a")
+      pipe.puts("GOOG")
+      pipe.puts("AAPL")
+      pipe.puts("MSFT")
+      pipe.puts("quit")
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    assert_includes shell_output, ""
+    assert_includes shell_output, "Would you like to add or delete stock tickers from this portfolio? [a/d]"
+    assert_includes shell_output, "Add a stock ticker:"
+    assert_includes shell_output, "Add another stock ticker (press 'quit' to exit):"
+    assert_includes shell_output, "
+1. GOOG
+2. AAPL
+3. MSFT"
+  end
+
+  def test_10_delete_tickers
+    Portfolio.create!(name: "Tech")
+    shell_output = ""
+    IO.popen('ruby rubystocks edit Tech', 'r+') do |pipe|
+      pipe.puts("d")
+      pipe.puts("GOOG")
+      pipe.puts("AAPL")
+      pipe.puts("MSFT")
+      pipe.puts("quit")
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    assert_includes shell_output, ""
+    assert_includes shell_output, "Would you like to add or delete stock tickers from this portfolio? [a/d]"
+    assert_includes shell_output, "Add a stock ticker:"
+    assert_includes shell_output, "Add another stock ticker (press 'quit' to exit):"
+    assert_includes shell_output, "
+1. GOOG
+2. AAPL
+3. MSFT"
+  end
+
+
+  # def test_11_delete_tickers
+  
   # end
 
-  # def test_add_ticker_creates_correct_name
-  #   portfolio = Ticker.create(name: "Tech")
-  #   `ruby rubystocks add Tech`
-  #   assert_equal("GOOG", portfolio.tickers.last.name)
-  # end
 
-  # def test_add_more_tickers
-  #   Ticker = Ticker.add(name: "GOOG")
-  #   Ticker = Ticker.add(name: "AAPL")
-  #   Ticker = Ticker.add(name: "MSFT")
-  #   assert_equal("Banks", ticker.name)
-  # end
+  # def test_12_view_portfolio_tickers
 
-  # def test_add_yet_another_Ticker
-  #   Ticker = Ticker.add(name: "Retail")
-  #   assert_equal("Retail", ticker.name)
   # end
 
 
-  # def test_list_all_portfolios
-  #   assert results.include?("Tech Banks"), true
-  # end
+
 end
